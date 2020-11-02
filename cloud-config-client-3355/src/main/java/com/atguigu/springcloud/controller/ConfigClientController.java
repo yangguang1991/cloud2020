129e2b58591fc871ae64b659e170e8abfba6e4af
yang <cqupt991@163.com>
2020-11-02 11:22:34 +0800

    config client代码提交

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @auther zzyy
  * @create 2020-02-21 18:08
+ * 修改完配置文件需要手动执行curl -X POST  "http://localhost:3355/actuator/refresh" ，来刷新配置，不用重启3355客户端即可获取github新的配置
+ * 这比重启客户端的代价要小很多
  */
 @RestController
-@RefreshScope
+@RefreshScope //自动刷新配置的功能，
 public class ConfigClientController
 {
     @Value("${config.info}")
